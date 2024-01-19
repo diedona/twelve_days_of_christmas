dc474beac206aa8b65981ff0e9f7e662f53e51ef
diedona <diedona@gmail.com>
2024-01-19 10:10:05 -0300

    first iteration

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
 fn main() {
-    let phrases: [&str; 4] = get_phrases_days_of_christmas();
-    let days: [u8; 12] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    let phrases: Vec<&str> = get_phrases_days_of_christmas();
+    let days: [u8; 4] = [1, 2, 3, 4];
 
     for day in days {
-        println!("{day}");
+        print_chorus(&day, &phrases);
     }
 }
 
-fn get_phrases_days_of_christmas<'a>() -> [&'a str; 4] {
-    return [
+fn print_chorus(day: &u8, phrases: &Vec<&str>) {
+    println!("On the {day} day of christmas,");
+    println!("my true love gave me...\n");
+
+    print_gifts(day, phrases);
+    println!("---");
+}
+
+fn print_gifts(day: &u8, phrases: &Vec<&str>) {
+    let mut current_day = *day as usize;
+    current_day -= 1;
+
+    loop {
+        let current_phrase = phrases
+            .get(current_day)
+            .expect("current_day out of range?");
+
+        if day > &1 && current_day == 0 {
+            println!("... and {}", current_phrase.to_lowercase());
+        } else {
+            println!("{current_phrase}");
+        }
+        
+
+        if current_day < 1 {
+            break;
+        }
+
+        current_day -= 1;
+    }
+}
+
+fn get_phrases_days_of_christmas<'a>() -> Vec<&'a str> {
+    return vec![
         "A partridge in a pear tree",
         "Two turtle doves",
         "Three French hens",
         "Four calling birds",
+        "Five golden rings",
+        "Six geese a-laying"
     ];
 }
