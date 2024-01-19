37616a8dcf4a56ba335b00768dbd0a207d4f6d88
diedona <diedona@gmail.com>
2024-01-19 12:55:06 -0300

    better names, more formatting

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,86 @@
 fn main() {
-    let phrases: Vec<&str> = get_phrases_days_of_christmas();
-    let days: [u8; 4] = [1, 2, 3, 4];
+    let gifts: Vec<&str> = get_gifts();
+    let days: [usize; 12] = core::array::from_fn(|day| day + 1);
 
     for day in days {
-        print_chorus(&day, &phrases);
+        print_chorus(&day, &gifts);
     }
 }
 
-fn print_chorus(day: &u8, phrases: &Vec<&str>) {
-    println!("On the {day} day of christmas,");
+fn print_chorus(day: &usize, gifts: &[&str]) {
+    println!("On the {} day of christmas,", format_day(day));
     println!("my true love gave to me...\n");
 
-    print_gifts(day, phrases);
+    print_gifts(day, gifts);
     println!("---");
 }
 
-fn print_gifts(day: &u8, phrases: &Vec<&str>) {
-    let mut current_day = *day as usize;
-    current_day -= 1;
+fn format_day(day: &usize) -> &str {
+    return match day {
+        1 => "first",
+        2 => "second",
+        3 => "third",
+        4 => "fourth",
+        5 => "fifth",
+        6 => "sixth",
+        7 => "seventh",
+        8 => "eighth",
+        9 => "nineth",
+        10 => "tenth",
+        11 => "eleventh",
+        12 => "twelvth",
+        _ => "oopsie",
+    };
+}
 
+fn print_gifts(day: &usize, gifts: &[&str]) {
+    if *day == 0 {
+        return;
+    }
+
+    let mut current_gift_idx = (*day - 1) as usize;
     loop {
-        let current_phrase = phrases
-            .get(current_day)
-            .expect("current_day out of range?");
-       
-        println!("{}", format_current_phrase(&current_phrase, &day, &current_day));
-        if current_day < 1 {
+        let current_gift = match gifts.get(current_gift_idx) {
+            Some(gift) => gift.to_string(),
+            None => format!("idx {} - an oopsie? gift out of range", current_gift_idx),
+        };
+
+        println!(
+            "{}",
+            format_current_gift_in_phrase(&current_gift, day, &current_gift_idx)
+        );
+        if current_gift_idx < 1 {
             break;
         }
 
-        current_day -= 1;
+        current_gift_idx -= 1;
     }
 }
 
-fn format_current_phrase<'a>(
-    current_phrase: &str,
-    day: &u8,
-    current_day: &usize
+fn format_current_gift_in_phrase<'a>(
+    current_gift: &String,
+    day: &usize,
+    current_gift_idx: &usize,
 ) -> String {
-
-    if day > &1 && current_day == &0 {
-        return format!("... and {}!", current_phrase.to_lowercase());
+    if *day == 1 && *current_gift_idx == 0 {
+        return format!("{}!!!", current_gift);
     }
 
-    return format!("{}, ", current_phrase);
+    if *day > 1 && *current_gift_idx == 0 {
+        return format!("... and {}!", current_gift.to_lowercase());
+    }
+
+    return format!("{},", current_gift);
 }
 
-fn get_phrases_days_of_christmas<'a>() -> Vec<&'a str> {
+fn get_gifts<'a>() -> Vec<&'a str> {
     return vec![
         "A partridge in a pear tree",
         "Two turtle doves",
         "Three French hens",
         "Four calling birds",
         "Five golden rings",
-        "Six geese a-laying"
+        "Six geese a-laying",
+        "Seven swans a-swimming",
     ];
 }
